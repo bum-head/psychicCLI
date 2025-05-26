11038aef188a97517f0d26bd0c1cc39863614fde
bum-head <isnotrealluck@gmail.com>
2025-05-26 07:54:57 +0530

    added Update funtion which updates the status of the task and the description of it

--- main.py
+++ main.py
@@ -49,7 +49,60 @@ def add():
         print("\n")
 
 def update():
-    pass
+    print("TO UPDATE DESCRIPTION \n \'update-desc [task-id] [updated-description]\'") #print the syntax of how to type the command
+    print("TO UPDATE TASK-PROGRESS \n Type progress,done and ndone as your task-state \n \'update-task [task-id] [updated-task-state]\'") #prints the syntax of updating tasks 
+
+    print("TO EXIT TYPE \'back\'") #show other options eg. exit update function
+
+
+    update_i = input().split()
+    print("")
+
+    if update_i[0] == "update-desc":
+        try:
+            file = open("tasks.json", "r+")
+            read_data = json.load(file)
+            new_file = open("temp.json", "w+")
+            for _ in read_data:
+                if _.get('ID') == int(update_i[1]):
+                    _["Description"] = update_i[2]
+            json.dump(read_data, new_file, indent = 4)
+            os.remove("tasks.json")
+            os.rename("temp.json", "tasks.json")
+        except FileNotFoundError:
+            print("Are you sure you have created a task to modify it ?")
+        except Error as e:
+            print(f"Err: {e}")
+
+    elif update_i[0] == "update-task":
+        try:
+            file = open("tasks.json", "r+")
+            read_data = json.load(file)
+            new_file = open("temp.json", "w+")
+            for _ in read_data:
+                if _.get("ID") == int(update_i[1]):
+                    if update_i[2] == "progress":
+                        _["Status"] = "In-Progress"
+                    elif update_i[2] == "done":
+                        _["Status"] = "Done!"
+                    elif update_i[2] == "ndone" or "notdone":
+                        _["Status"] = "To-Do"
+                    else:
+                        print("Incorrect option")
+            json.dump(read_data, new_file, indent = 4)
+            os.remove("tasks.json")
+            os.rename("temp.json", "tasks.json")
+        except FileNotFoundError:
+            print("Are you sure you have created a task to modify it ?")
+        except Error as e:
+            print(f"Err: {e}")
+
+
+    elif update_i == "back":
+        return
+    else:
+        print("Invalid option!\n Please refer to the instructions")
+
 
 def delete():
     pass
@@ -85,15 +138,19 @@ def listLive():
 print("TaskManCLI v1.0")
 while True:
     print("Enter Operation to perform")
-    des = input("\n1.Add Task\n2.List All Tasks\n3.Coming Soon..\n")
+    des = input("\n1.Add Task\n2.List All Tasks\n3.Update Tasks\n4.Exit\n")
+    print("")
     try:
         if 1 == int(des) :
             add()
         elif 2 == int(des):
             listAll()
+        elif 3 == int(des):
+            update()
         else:
             break
     except ValueError:
         print("Invalid Option!!")
         print("\n")
+
 
